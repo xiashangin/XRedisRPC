210476fa88c98f6526010916e3189a9c3618eec3
Hexu <hx@singula-point.com>
2017-12-04 14:35:33 +0800

    测试例子

--- XglRedisTest/XglRedisTest.cpp
+++ XglRedisTest/XglRedisTest.cpp
@@ -1,46 +1,48 @@
 // XglRedisTest.cpp : 定义控制台应用程序的入口点。
 //
 
-#include "stdafx.h"
-
 #include "Redis_Utils.h"
 #pragma comment(lib, "libXglRedis.lib")
 
-void subsCb1(const char *key)
+void subsCb1(const char *key, const char *value)
 {
-	cout << "subsCb1 get respond-->" << key << endl;
-}
-void subsCb2(const char *key)
-{
-	cout << "subsCb2 get respond-->" << key << endl;
+	DEBUGLOG << "key = " << key << ", value = " << value << endl;
 }
 
-
-void pullCb1(const char *key)
+void subsCb2(const char *key, const char *value)
 {
-	cout << "pullCb1 get respond-->" << key << endl;
+	DEBUGLOG << "key = " << key << ", value = " << value << endl;
 }
 
-void pullCb2(const char *key)
+int main(int argc, char const *argv[])
 {
-	cout << "pullCb2 get respond-->" << key << endl;
-}
+	CRedis_Utils redis("aclient");
+	redis.connect("192.168.253.128", 6379, true);
+	//redis.disconnect();
+	//redis.connect("192.168.253.128", 6379, true);
 
-int main()
-{
-	CRedis_Utils redis;
-	redis.connect("192.168.31.170", 6379);
-	char msg[256];
-	memset(msg, 0, 256);
+	//????sub 和 pull 订阅相同的情况！！！！
+	//redis.subs("hello", subsCb1);
+	redis.pull("hexu", subsCb2);
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
-	redis.subs("hello?", subsCb1);
-	redis.subs("hello*ac*", subsCb2);
+	//正常基本功能
+	char *msg = (char *)malloc(REDIS_BUF_SIZE);
+	//memset(msg, 0, REDIS_BUF_SIZE);
+	//DEBUGLOG << "rlt = " << redis.set("hello", "dsklfjfdisewrkdkjfshf", msg) << "..." << msg << endl;
+	//memset(msg, 0, REDIS_BUF_SIZE);
+	//DEBUGLOG << "rlt = " << redis.set("hello", "dsklfjfdisewrkdkjfshf", msg) << "..." << msg << endl;
+	//memset(msg, 0, REDIS_BUF_SIZE);
+	//DEBUGLOG << "rlt = " << redis.get("hexu", msg) << "..." << msg << endl;
+	memset(msg, 0, REDIS_BUF_SIZE);
+	DEBUGLOG << "rlt = " << redis.push("hexu", "bilibili", msg) << "..." << msg << endl;
+	//memset(msg, 0, REDIS_BUF_SIZE);
+	//DEBUGLOG << "rlt = " << redis.pop("kdjshfkjsdf1", msg) << "..." << msg << endl;
 
-	redis.pull("hello?", pullCb1);
-	redis.pull("hello*ac*", pullCb2);
+
+	//redis.pull("hello", subsCb1);
+	//redis.pull("hello", subsCb2);
 
 	getchar();
-	redis.close();
-    return 0;
+	return 0;
 }
-
