ad008b14d8a3e94018132d3dc0bc6d9f11f13a59
Hexu <hx@singula-point.com>
2017-12-13 17:59:08 +0800

    接口说明，文档补充。

--- libXglRedis/Redis_Utils.h
+++ libXglRedis/Redis_Utils.h
@@ -2,13 +2,6 @@
 
 #include "RedisRPC.h"
 
-//#ifdef _WIN32
-//#include <WinSock2.h>
-//#else
-//#include <sys/time.h>
-//#endif // _WIN32
-
-
 typedef void(*subsCallback)(const std::string & strKey, const std::string & strValue);
 typedef void(*pullCallback)(const std::string & strKey, const std::string & strValue);
 typedef void(*clientOpCallBack)(const std::string & strKey, const std::string & strValue);
@@ -26,7 +19,7 @@ public:
 	//客户端基本操作函数
 	/*
 	isSubs说明：默认false
-		false：不使用redis的键空间通知功能，即subs、pull、subsClientGetOp等接口不起作用
+		false：不使用redis的键空间通知功能，即subs、pull、subsClientGetOp等接口不起作用，只能进行redis基本操作
 		true：启用redis的键空间通知功能
 	*/
 	bool connect(const std::string & strIp, int iPort, bool bNeedSubs = false);		//连接redis服务并订阅redis键空间通知
@@ -34,16 +27,10 @@ public:
 
 	//redis基本操作
 	/**
-		get()    pop()
 		返回值说明：
-			-1：操作失败，失败原因通过lpStrRlt查看
-			 0：lpStrKey对应的值为空
-			>0：操作成功，lpStrKey的值通过lpStrRlt查看，返回lpStrRlt的长度
-
-		set()    push()
-		返回值说明：
-			true：操作成功，结果通过lpStrRlt查看，一般为OK
-			false：操作失败，结果通过lpStrRlt查看
+			0：操作成功
+			>0：操作失败，返回状态码
+		操作结果通过strOutResult获取
 	*/ 
 	int get(const std::string & strInKey, std::string & strOutResult);
 	int set(const std::string & strInKey, const std::string & strInValue, std::string & strOutResult);
@@ -51,17 +38,42 @@ public:
 	int pop(const std::string & strInListName, std::string & strOutResult);
 
 	//redis订阅功能
+	/*
+	subs()	pull()
+	返回值说明：
+		0：操作成功
+		>0：操作失败，返回状态码
+
+	unsubs()	unpull()
+	返回值说明：
+		true：操作成功
+		false：操作失败
+	*/
 	int subs(const std::string & strInKey, subsCallback cb);	//subscribe channel
 	bool unsubs(const std::string & strInKey);					//unsubscribe channel
 	int pull(const std::string & strInKey, pullCallback cb);	//pull list
 	bool unpull(const std::string & strInKey);					//unpull list, like unsubs
 
 	//业务处理模块
+	/*
+	subsClientGetOp()
+	返回值说明：
+		0：操作成功
+		>0：操作失败，返回状态码
+	
+	notifyRlt()
+	返回值说明：
+		0：操作成功
+		>0：操作失败，返回状态码
+	作用：处理完成请求之后通过此函数通知客户端
+	*/
 	int subsClientGetOp(const std::string & strInKey, clientOpCallBack cb);
 	bool unsubClientGetOp(const std::string & strInKey);							//注销监听客户端get key操作
 	void stopSubClientGetOp();														//取消监听客户端全部get操作
 	int notifyRlt(const std::string & strInKey, const std::string & strInValue);	//通知客户端处理完成
 private:
+	CRedis_Utils(const CRedis_Utils & c);	//不允许拷贝
+	
 	void close();
 	bool _connect(const std::string & strIp, int iPort);
 	//实现业务模块的数据隔离
@@ -78,7 +90,6 @@ private:
 	bool getReq(std::string strInkey);
 
 	void callSubsCB(const std::string & strInKey, const std::string & strInOp);
-	CRedis_Utils(const CRedis_Utils & c);
 
 	redisContext *m_pRedisContext;			//redis同步上下文
 	//std::shared_ptr<redisContext *> m_spRedisContext;
